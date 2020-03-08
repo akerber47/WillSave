b1d09c23388e68015a0524e85967888355db533e
Alvin Kerber <alvin@alvinkerber.net>
2020-03-08 15:10:02 -0700

    basic webview with safari delegate

--- WillSave/FirstViewController.swift
+++ WillSave/FirstViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+import SafariServices
+import WebKit
 
+class FirstViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.navigationDelegate = self
+        let url = URL(string:"https://alvinkerber.net")
+        webView.load(URLRequest(url: url!))
+    }
+    
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
+        if navigationAction.navigationType == .linkActivated {
+            if let url = navigationAction.request.url {
+                let svc = SFSafariViewController(url: url)
+                self.present(svc, animated: true)
+                decisionHandler(.cancel)
+                return
+            }
+        }
+        decisionHandler(.allow)
     }
 
     override func didReceiveMemoryWarning() {
