48a84b8d543f6c3830de8d2802fd5a0709b85a6d
Alvin Kerber <alvin@alvinkerber.net>
2020-03-08 14:25:00 -0700

    Initial Commit

---
+++ WillSave/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WillSave
+//
+//  Created by alvin on 3/8/20.
+//  Copyright © 2020 Ice Nine Labs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
